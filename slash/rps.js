9608f54652fc43c15b5126c6f0ef11296310b132
Indiance <tharun.tvishy@gmail.com>
2022-02-26 23:05:20 +0530

    removing unecessary file space in mute.js and fixing user input in rock paper scissors.

--- slash/rps.js
+++ slash/rps.js
@@ -1,7 +1,10 @@
 // importing the necessary stuff
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const {MessageActionRow, MessageButton } = require('discord.js');
-
+// sleep function
+function sleep() {
+    return new Promise(resolve => setTimeout(resolve, 2000));
+}
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('rps')
@@ -9,7 +12,7 @@ module.exports = {
 	async execute(interaction) {
 		// initiating a list of elements and making the opponent pick a random one
 		const elements = ['rock', 'paper', 'scissors'];
-		const chosen = Math.floor(Math.random() * elements.length);
+		const chosen = elements[Math.floor(Math.random() * elements.length)];
 		// setting up a row with button
 		const row = new MessageActionRow()
 		// adding the various buttons for player to choose
@@ -32,15 +35,18 @@ module.exports = {
 					.setStyle('PRIMARY'),
 			);
 		// let the user pick
-		interaction.reply({ content: "Pick your poison", components: [row] });
-
+		interaction.reply({ content: "Pick your poison", components: [row], ephemeral: true});
 		// get the button click only from the user that initiated the message (that is why the filter thing exists)
-		const filter = i => i.user.id === i.member.id;
-		const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
-
+		const collector = interaction.channel.createMessageComponentCollector({ max: 1, time: 20000 });
 		// what to do if button click in given time period
 		collector.on('collect', async i => {
-			await i.update({ content: 'A button was clicked!', components: [] });
+			await i.reply({ content: 'A choice was made!', components: [] });
+			await sleep();
+			await i.editReply({ content: 'The opponent chose something as well!', components: []});
+			/*
+			TODO: Work on a function to check for who wins what.
+			Make this a separate function for readability
+			*/
 		});
 		collector.on('end', collected => console.log(`Collected ${collected.size} items`));
 	}
